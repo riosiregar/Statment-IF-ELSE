4f558f04823ca3f0de6aedeada9263777e5a19b6
riosiregar <37629273+riosiregar@users.noreply.github.com>
2020-04-05 12:25:19 +0700

    Add files via upload

---
+++ BP-Materi-If-Else.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int nilai;
+	cout<<"Input Nilai MatKul:"<<endl;
+	cin>>nilai;
+	
+	if (nilai>=60){
+		cout<<"Lulus";
+	}
+	else{
+		cout<<"CIE GAK LULUS";
+	}
+	return 0;
+}
