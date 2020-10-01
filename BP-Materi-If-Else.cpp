3f75c740f029d881e20bbba18a3fd293032fa394
CodingWithAmit <72186433+CodingWithAmit-07@users.noreply.github.com>
2020-10-01 14:03:59 +0530

    Update BP-Materi-If-Else.cpp

--- BP-Materi-If-Else.cpp
+++ BP-Materi-If-Else.cpp
@@ -1,3 +1,4 @@
+//Code For If else Statement
 #include <iostream>
 
 using namespace std;
@@ -8,11 +9,12 @@ int main()
 	cout<<"Input Nilai MatKul:"<<endl;
 	cin>>nilai;
 	
-	if (nilai>=60){
+	if (nilai>=60){          //If nilai will be >=60 then it will print Luslus
 		cout<<"Lulus";
 	}
-	else{
+	else{                    //If nilai will not be >=60 then it will print CIE GAK LULUS  
 		cout<<"CIE GAK LULUS";
 	}
 	return 0;
 }
+//**code end**
